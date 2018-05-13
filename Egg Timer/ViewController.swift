334f73507e0b8f110ccf251ac88e50c2e0ab9bd9
Brent Mifsud <brent.mifsud@scotiabank.com>
2018-05-13 15:43:13 -0400

    Initial Commit

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Mifsud, Brent on 2018-05-13.
+//  Copyright © 2018 Brent Mifsud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
